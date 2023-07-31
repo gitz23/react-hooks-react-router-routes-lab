aad7e3bc964f749dd76709753111ab48183c33d8
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-31 20:44:43 +0300

    done with navbar test

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
@@ -7,7 +7,14 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+      <NavBar />
+      <Route exact path="/movies" element={<Movies />}/>
+      <Route exact path="/directors" element={<Directors />}/>
+      <Route exact path="/actors" element={<Actors />}/>
+      <Route exact path="/" element={<Home />}/>
+
+    </div>;
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,25 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+
+  const linkStyles = {
+    display: "inline-block",
+    width: "70px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  };
+
+  return (
+     <div className="navbar">
+      <NavLink exact to="/" style={linkStyles}>Home</NavLink>
+      <NavLink exact to="/directors" style={linkStyles}>Directors</NavLink>
+      <NavLink exact to="/actors" style={linkStyles}>Actors</NavLink>
+      <NavLink exact to="/movies" style={linkStyles}>Movies</NavLink>
+    </div>
+    )
 }
 
 export default NavBar;
